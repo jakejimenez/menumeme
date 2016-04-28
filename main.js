9d34f9571c555e0f933d16f755b1f02e4acf0a87
r00t1000 <jacob.jime@gmail.com>
2016-04-28 16:11:15 -0700

    added crap

--- main.js
+++ main.js
@@ -1,11 +1,82 @@
 // Initial Variables
 var menubar = require('menubar');
+var Menu = require('menu');
+var ipc = require('electron').ipcMain;
+var globalShortcut = require('global-shortcut');
 
 // The Window is set to nothing. Clean Slate.
-var mb = menubar()
+var mb = menubar({
+  dir: __dirname,
+  width: 450,
+  height: 450,
+  icon: __dirname + '/assets/favicon.png',
+  preloadWindow: true
+});
+// When the menubar is ready to run.
 mb.on('ready', function ready() {
-  console.log('Ready!')
-})
+  console.log('Ready!');
+});
+
+mb.on('show', function () {
+  mb.window.webContents.send('show');
+});
+
+mb.app.on('activate', function () {
+  mb.showWindow();
+});
+
+ipc.on('abort', function () {
+  mb.hideWindow();
+});
+
+var template = [
+  {
+    label: 'MenuMeme',
+    submenu: [
+      {
+        label: 'Copy',
+        accelerator: 'CommandOrCtrl+C',
+        selector: 'copy:'
+      },
+      {
+        label: 'Paste',
+        accelerator: 'CommandOrCtrl+V',
+        selector: 'paste:'
+      },
+      {
+        label: 'Select All',
+        accelerator: 'CommandOrCtrl+A',
+        selector: 'selectAll:'
+      },
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click: function (item, focusedWindow) { if (focusedWindow) focusedWindow.reload() }
+      },
+      {
+        label: 'Preferance',
+        accelerator: 'CommandOrCtrl+,',
+        click: function () { mb.window.webContents.send('open-preference') }
+      },
+      {
+        label: 'Quit App',
+        accelerator: 'CommandOrCtrl+Q',
+        selector: 'terminate:'
+      },
+      {
+        label: 'Toggle DevTools',
+        accelerator: 'Alt+CommandOrCtrl+I',
+        click: function () { mb.window.toggleDevTools() }
+      }
+    ]
+  }
+];
+
+mb.on('ready', function ready () {
+  // Build default menu for text editing and devtools. (gone since electron 0.25.2)
+  var menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+});
 // This code is not needed, as this is a menubar application.
 // app.on('ready', function() {
 //  mainWindow = new BrowserWindow({
